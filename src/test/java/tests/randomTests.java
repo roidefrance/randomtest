519a3468b0240c105b61afcc31624f7c4852e800
roidefrance <kirmaxmel@gmail.com>
2021-02-01 11:19:32 +0300

    second commit new test

---
+++ src/test/java/tests/randomTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+class googleTests {
+
+
+    @Test
+    void siteExists() {
+
+        open("https://www.google.com/");
+        $("html").shouldHave(text("Почта"));
+    }
+
+}
