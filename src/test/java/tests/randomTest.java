5461efac80a4c8da5e45353913dab6a14b2e33a6
roidefrance <kirmaxmel@gmail.com>
2021-02-04 16:18:25 +0300

    4th commit added more code

--- src/test/java/tests/randomTest.java
+++ src/test/java/tests/randomTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,14 +13,21 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class randomTest {
+    @BeforeAll
+    static void setup(){
+    Configuration.timeout = 10000;
 
+    }
     @Test
     void siteExists() {
 
         open("https://www.mail.ru/");
         $("html").shouldHave(text("Почта"));
         $("html").shouldHave(text("Спорт"));
-$("#q").setValue("спорт").pressEnter();
+        $(byLinkText("Спорт")).doubleClick();
+        switchTo().window(1);
+        $(byText("Футбол")).hover();
+        $(byLinkText("Премьер-лига")).click();
 
     }
 }
